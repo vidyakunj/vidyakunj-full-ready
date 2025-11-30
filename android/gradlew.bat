364d4a2c219bb521bc501f42f3ea03b097bef7d3
Vidyakunj School Navsari <vidyakunjnavsari1969@gmail.com>
2025-11-30 18:29:25 +0530

    Add files via upload

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -1,14 +1,11 @@
 @echo off
 set DIR=%~dp0
-
 set JAVA_EXE=java.exe
 if not defined JAVA_HOME goto findJava
 set JAVA_EXE=%JAVA_HOME%\bin\java.exe
-
 :findJava
 if exist "%JAVA_EXE%" goto execGradle
 echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 exit /B 1
-
 :execGradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% -classpath "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
