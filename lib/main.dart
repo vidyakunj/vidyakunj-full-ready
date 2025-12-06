860f3b5de411806dc8a1712c2c5032d6bdfabd65
Vidyakunj School Navsari <vidyakunjnavsari1969@gmail.com>
2025-12-06 22:27:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
-import 'screens/new_attendance_screen.dart';
-import 'splash_check.dart';  // <--- add this
+import 'splash_check.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,46 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vidyakunj Attendance',
-
-      theme: ThemeData(
-  useMaterial3: true,
-
-  primaryColor: primaryNavy,
-
-  colorScheme: ColorScheme.fromSeed(
-    seedColor: primaryNavy,
-    brightness: Brightness.light,
-  ),
-
-  scaffoldBackgroundColor: const Color(0xfff6f8ff),
-
-  appBarTheme: const AppBarTheme(
-    backgroundColor: primaryNavy,
-    foregroundColor: Colors.white,
-    centerTitle: true,
-    elevation: 3,
-  ),
-
-  elevatedButtonTheme: ElevatedButtonThemeData(
-    style: ElevatedButton.styleFrom(
-      backgroundColor: primaryNavy,
-      foregroundColor: Colors.white,
-      padding: EdgeInsets.symmetric(vertical: 14),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(12)),
-      ),
-    ),
-  ),
-),
-
-      // -----------------------------------------------------
-      // First time → Login Screen
-      // Next time → Auto Login
-      // -----------------------------------------------------
-      home: const SplashCheck(),
-
       debugShowCheckedModeBanner: false,
+
+      home: const SplashCheck(),
     );
   }
 }
-
