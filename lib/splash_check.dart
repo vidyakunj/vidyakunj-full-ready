79ef84d7d2995c6c27d4064c0c9981e0eb7bd8b4
Vidyakunj School Navsari <vidyakunjnavsari1969@gmail.com>
2025-12-06 16:37:48 +0530

    Update splash_check.dart

--- lib/splash_check.dart
+++ lib/splash_check.dart
@@ -32,9 +32,11 @@ class _SplashCheckState extends State<SplashCheck> {
       );
     } else {
       if (role == "teacher") {
-        Navigator.pushReplacement(
-          context,
-          MaterialPageRoute(builder: (_) => const NewAttendanceScreen()),
+       Navigator.pushReplacement(
+         context,
+         MaterialPageRoute(builder: (_) => const TeacherDashboard()),
+);
+
         );
       } else {
         Navigator.pushReplacement(
