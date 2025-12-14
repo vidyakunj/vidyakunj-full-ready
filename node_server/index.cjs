bb6f1eb089df4310b315d5605a4827d70e8618fc
Vidyakunj School Navsari <vidyakunjnavsari1969@gmail.com>
2025-12-14 22:14:48 +0530

    Update index.cjs

--- node_server/index.cjs
+++ node_server/index.cjs
@@ -124,7 +124,7 @@ app.post("/attendance", async (req, res) => {
           params: {
             method: "SendMessage",
             send_to: e.mobile,
-            msg: `Dear Parents, Your child ${e.name} remained absent in school today.,Vidyakunj School`;
+            msg: `Dear Parents, Your child ${e.name} remained absent in school today.,Vidyakunj School`,
             msg_type: "TEXT",
             userid: process.env.GUPSHUP_USER,
             password: process.env.GUPSHUP_PASSWORD,
