050f9570680536607b552c804bc6e344d991b8f1
Vidyakunj School Navsari <vidyakunjnavsari1969@gmail.com>
2026-01-12 16:04:25 +0530

    Refactor server URL configuration comments

--- lib/config.dart
+++ lib/config.dart
@@ -5,9 +5,10 @@
 // 🔴 CHANGE THIS TO YOUR SERVER URL IF NEEDED
 // Do NOT put "/" at the end.
 const String SERVER_URL = "https://vidyakunj-backend.onrender.com";
-//const String SERVER_URL = "https://vidyakunj-sms-server.onrender.com";
+const String SERVER_URL = "https://vidyakunj-sms-server.onrender.com";
 
 // You can switch servers easily if needed:
 // const String SERVER_URL = "https://vidyakunj-server.onrender.com";
 // const String SERVER_URL = "http://localhost:3000";
+
 
