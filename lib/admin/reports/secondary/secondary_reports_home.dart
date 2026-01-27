fb710e8b096c239f534fff05a8c00877d566af8b
Vidyakunj School Navsari <vidyakunjnavsari1969@gmail.com>
2026-01-27 14:57:21 +0530

    Update secondary_reports_home.dart

--- lib/admin/reports/secondary/secondary_reports_home.dart
+++ lib/admin/reports/secondary/secondary_reports_home.dart
@@ -24,7 +24,7 @@ class _SecondaryReportsHomeState
 
     Future<void> loadSecondarySectionSummary() async {
     setState(() {
-      ing = true;
+    loading = true;
       hasData = false;
     });
 
